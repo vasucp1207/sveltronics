2e831b2f610c4d81d2815994d0f3623e8f55e296
Vasu <vasucp1207@gmail.com>
2023-03-15 10:29:00 +0530

    click outside

--- src/lib/clickOutside/demo.svelte
+++ src/lib/clickOutside/demo.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div use:clickOutside class="m-5 h-[100px] w-[100px] bg-[#ff3e00] border-2 border-black 
-  rounded text-white flex justify-center items-center cursor-grab"
+  rounded text-white flex justify-center items-center"
 >
   Outside
 </div>
--- src/lib/clickOutside/index.ts
+++ src/lib/clickOutside/index.ts
@@ -1,6 +1,6 @@
-export function clickOutside(node) {
-  const handleClick = (event) => {
-    if(!node.contains(event.target)) {
+export function clickOutside(node: HTMLElement) {
+  const handleClick = (event: MouseEvent) => {
+    if(node !== event.target) {
       alert("outside click 🥳");
     }
   }
