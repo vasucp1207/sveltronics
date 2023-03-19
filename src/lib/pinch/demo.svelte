063cc57da97586f31fb1857242c93b42eeecd302
Vasu <vasucp1207@gmail.com>
2023-03-19 12:19:26 +0530

    add pinch gesture

---
+++ src/lib/pinch/demo.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { pinch } from "./index";
+
+</script>
+
+<div class="flex flex-col items-center justify-center h-96">
+  <div 
+    use:pinch
+    class="h-[100px] w-[100px] bg-[#ff3e00] border-2 rounded text-white flex 
+    justify-center items-center cursor-grab"
+  >
+    Pinch Me
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Demo from "$lib/localStorage/demo.svelte";
+  import Demo from "$lib/pinch/demo.svelte";
 
 </script>
 
