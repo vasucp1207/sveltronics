9826326759b08a5a669e0f1b5bca62ed02abe63b
Vasu <vasucp1207@gmail.com>
2023-03-25 14:53:52 +0530

    add use async state

---
+++ src/lib/asyncState/index.ts
@@ -0,0 +1,31 @@
+import { writable } from "svelte/store";
+
+type asyncRequest = Promise<Response>;
+
+interface Store {
+  subscribe: any;
+  set: (value: any) => void;
+  update: (fn: (value: any) => any) => void;
+}
+
+export async function asyncState(request: asyncRequest) {
+  let state = writable<string>('idle');
+  let value = writable<string>();
+  let isLoading = writable<boolean>(true);
+
+  try {
+    let res = await request.then(res => res.json());
+    state.set('resolved');
+    value.set(JSON.stringify(res));
+    isLoading.set(false);
+  } catch (error) {
+    state.set(`${error}`);
+    isLoading.set(false);
+  }
+
+  return {
+    state,
+    value,
+    isLoading
+  };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Demo from "$lib/longPress/demo.svelte";
+  import Demo from "$lib/asyncState/demo.svelte";
 
 </script>
 
