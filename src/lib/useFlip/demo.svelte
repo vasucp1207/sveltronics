4d7416fce4f975a0a3ba069ede518af8f64f203b
Vasu <vasucp1207@gmail.com>
2023-03-30 18:40:22 +0530

    add useFlip

---
+++ src/lib/useFlip/demo.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { useFlip } from "./index";
+
+  // let { front, flip } = useFlip();
+
+  // $: console.log($front, 'front', flip)
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  use:useFlip
+  class="flip m-32 rounded h-80 w-60 text-white flex justify-center items-center"
+>
+  <div class="content">
+    <div class="front">
+      <img
+        class="h-80 w-60 object-cover"
+        src="https://tse1.mm.bing.net/th?id=OIP.VRFtOMu9MHIS-41nstIxJgHaE8&pid=Api&P=0"
+        alt="img"
+      />
+    </div>
+    <div class="back flex justify-center p-2">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat ex
+        officia illum aspernatur id impedit labore quos vitae. Cumque ea fugit
+        harum, aliquid dolores voluptatem pariatur odit obcaecati non!
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Demo from "$lib/parallax/demo.svelte";
+  import Demo from "$lib/useFlip/demo.svelte";
 
 </script>
 
